5081983d2ce4e1292b00b0cf29a3a89357765ab8
n00tn8 <nathanvlaere@gmail.com>
2025-11-23 00:40:42 +0100

    upgraded rustiness

--- NetworkedDB/src/unit_converter.rs
+++ NetworkedDB/src/unit_converter.rs
@@ -1,4 +1,5 @@
-// Convert SI prefixes
+
+// Convert SI prefixes to the factor needed to remove them: k -> 1e3
 fn convert_prefixes(prefix: &str) -> f64 {
     return match prefix {
         "p" => 1e-12,
@@ -13,11 +14,12 @@ fn convert_prefixes(prefix: &str) -> f64 {
     };
 }
 
-// Simply fully written SI names to abbreviations
+// Simplify fully written SI names to abbreviations
 pub fn simplify_input(input: &str) -> String {
-    let temp = input;
-    let temp = str::replace(&temp, "Hertz","Hz");
+    println!("Asked to simplify input of {}.",input);
+    let temp = str::replace(&input, "Hertz","Hz");
     let temp = str::replace(&temp, "meter","m");
+    let temp = str::replace(&temp, "gram","g");
     
     let temp = str::replace(&temp, "Tera","T");
     let temp = str::replace(&temp, "Giga","G");
@@ -30,6 +32,7 @@ pub fn simplify_input(input: &str) -> String {
     return temp;
 }
 
+
 // Convert supported values by unit name
 pub fn convert_units(value: f64, from_unit: &str, to_unit: &str) -> f64 {
 
@@ -39,21 +42,64 @@ pub fn convert_units(value: f64, from_unit: &str, to_unit: &str) -> f64 {
 
     let from_unit = simplify_input(from_unit);
     let to_unit = simplify_input(to_unit);
-    if from_unit[from_unit.len()-2..] == *"Hz" && to_unit[from_unit.len()-2..] == *"Hz"{
-        if from_unit.len() == 2 {
-            return value / convert_prefixes(&to_unit[..to_unit.len()-2]);
+
+    let si_unit_length:usize = 2;
+    let second_to_last_from:usize = from_unit.len()-si_unit_length;
+    let second_to_last_to:usize    = to_unit.len()-si_unit_length;
+
+    // TODO allow other easy units, more generally
+    if from_unit[second_to_last_from..] == *"Hz" && to_unit[second_to_last_to..] == *"Hz"{
+        
+        println!("Recognised that Hz and Hz were present");
+        if from_unit.len() == si_unit_length {
+            return value / convert_prefixes(&to_unit[..second_to_last_to]);
 
         }
-        else if to_unit.len() == 2 {
-            return value * convert_prefixes(&from_unit[..from_unit.len()-2]);
+        else if to_unit.len() == si_unit_length {
+            return value * convert_prefixes(&from_unit[..second_to_last_from]);
         }
         else {
-            return (value * convert_prefixes(&from_unit[..from_unit.len()-2])) / convert_prefixes(&to_unit[..to_unit.len()-2]);
+            return (value * convert_prefixes(&from_unit[..second_to_last_from])) / convert_prefixes(&to_unit[..second_to_last_to]);
         }
     }
     else{
+        // TODO: log INFO, don't just print
         println!("Conversion from {} to {} not yet supported.",from_unit,to_unit);
         return value;
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_convert_prefixes() {
+        assert_eq!(convert_prefixes("G"), 1e9);
+        assert_eq!(convert_prefixes("unknown"), 1.0);
+        assert_eq!(convert_prefixes("k"), 1e3);
+    }
+
+    #[test]
+    fn test_simplify_input() {
+        assert_eq!(simplify_input("GHertz"), "GHz");
+        assert_eq!(simplify_input("GigaHertz"), "GHz");
+        assert_eq!(simplify_input("GigaHz"), "GHz");
+        assert_eq!(simplify_input("kilo"), "k");
+        assert_eq!(simplify_input("kilogram"), "kg");
+        assert_eq!(simplify_input("unknown"), "unknown");
+    }
+
+    #[test]
+    fn test_convert_units() {
+        assert_eq!(convert_units(3.0, "unknown","other"), 3.0);
+        assert_eq!(convert_units(3.0, "unknown","unknown"), 3.0);
+        assert_eq!(convert_units(3.0, "Hertz","unknown"), 3.0);
+        assert_eq!(convert_units(3.0, "TeraHertz","THz"), 3.0);
+        assert_eq!(convert_units(3.0, "kHz","Hz"), 3e3);
+        assert_eq!(convert_units(3.0, "Hz","mHz"), 3e3);
+        assert_eq!(convert_units(3.0, "mHz","Hz"), 3e-3);
+        assert_eq!(convert_units(3.0, "kHz","THz"), 0.000000003);
+    }
 }
---
+++ NetworkedDB/target/release/deps/NetworkedDB-5dbcfebd10d4d0d7.d
@@ -0,0 +1,6 @@
+/home/nathanvl/dev/RestfulRust/NetworkedDB/target/release/deps/NetworkedDB-5dbcfebd10d4d0d7.d: src/main.rs src/unit_converter.rs
+
+/home/nathanvl/dev/RestfulRust/NetworkedDB/target/release/deps/NetworkedDB-5dbcfebd10d4d0d7: src/main.rs src/unit_converter.rs
+
+src/main.rs:
+src/unit_converter.rs:
